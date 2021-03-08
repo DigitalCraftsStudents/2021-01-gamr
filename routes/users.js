16d6e7084b10eb58e91886aa115d7dbfafbb531c
Lachlan Heywood <lachlan@heywood.net.au>
2021-03-08 10:50:03 -0500

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../models');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  db.User.findAll()
+    .then(users => {
+      res.json(users)
+    })
+});
+
+router.post('/', (req, res) => {
+  db.User.create({
+    name: req.body.name,
+    email: req.body.email,
+    gamerTag: req.body.gamerTag,
+  })
+    .then(user => {
+      res.json(user);
+    })
+    .catch(error => {
+      res.json(error.errors.map(e => e.message))
+    })
+})
+
+module.exports = router;
